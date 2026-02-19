36f82b3183b420037c5ef144b90b7451daf90dda
Penina <wpenina@gmail.com>
2026-02-19 21:32:22 +0300

    Initial commit

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:3001";
+export const API_BASE =
+    import.meta.env.VITE_API_BASE || "https://scms-api-production.up.railway.app";
